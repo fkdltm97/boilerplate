1ab0cfc5d3ca07ad1c6e2909abbf0902125bb46f
fkdltm97 <fkdltm97@daum.net>
2020-07-01 09:59:06 +0900

    node완료

--- models/User.js
+++ models/User.js
@@ -84,6 +84,21 @@ userSchema.methods.generateToken = function(cb){
     })
 }
 
+userSchema.statics.findByToken = function(token,cb){
+    var user = this;
+
+    //토큰을 decode 한다
+
+    jwt.verify(token, 'secretToken', function(err,decode){
+
+
+        user.findOne({"_id":decoded,"token":token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user)
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
